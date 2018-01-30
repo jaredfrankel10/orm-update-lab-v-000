ef7413fb2b7de7b818cac59f063a716d2acb5df9
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 14:45:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,46 +6,46 @@ class Student
   #  with DB[:conn]
   attr_accessor :name, :grade
      attr_reader :id
-    
+
      def initialize(name, grade, id=nil)
        @name = name
        @grade = grade
        @id = id
      end
-    
+
      def save
        if self.id
          self.update
        end
-   
+
        sql = <<-SQL
-         INSERT INTO students (name, grade) 
+         INSERT INTO students (name, grade)
          VALUES (?, ?)
          SQL
-   
+
        DB[:conn].execute(sql, self.name, self.grade)
-   
+
        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
      end
-   
+
      def update
        sql = <<-SQL
          UPDATE students SET name = ?, grade = ? WHERE id = ?
          SQL
-   
+
        DB[:conn].execute(sql, self.name, self.grade, self.id)
      end
-   
+
      def self.create(name, grade)
        student = Student.new(name, grade)
        student.save
      end
-   
+
      def self.new_from_db(row)
        student = Student.new(row[1], row[2], row[0])
        student
      end
-   
+
      def self.find_by_name(name)
        sql = <<-SQL
          SELECT * FROM students
@@ -54,7 +54,7 @@ class Student
        row = DB[:conn].execute(sql, name)
        Student.new_from_db(row.first)
      end
-   
+
      def self.create_table
        sql = <<-SQL
          CREATE TABLE IF NOT EXISTS students (
@@ -63,13 +63,13 @@ class Student
          grade INTEGER
          )
          SQL
-   
+
        DB[:conn].execute(sql)
      end
-   
+
      def self.drop_table
        sql = "DROP TABLE IF EXISTS students"
-   
+
        DB[:conn].execute(sql)
      end
 
